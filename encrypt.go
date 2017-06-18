666468acb0647e7b574f05eba4bced9bf89b2e87
Anas Khan <anas.khan96@outlook.com>
2017-06-19 04:38:08 +0530

    added default key length and made generateSalt() public

--- encrypt.go
+++ encrypt.go
@@ -2,10 +2,11 @@ package passwordEncryptor
 
 import "crypto/rand"
 
-const defaultLen = 256
+const defaultSaltLen = 256
 const defaultIterations = 10000
+const defaultKeyLen = 512
 
-func generateSalt(length int) string {
+func GenerateSalt(length int) string {
 	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	salt := make([]byte, length)
 	rand.Read(salt)
