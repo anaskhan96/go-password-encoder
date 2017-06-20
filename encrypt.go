ffc1eb0e381b12ac800af1060e0da850ae70b3c3
Anas Khan <anas.khan96@outlook.com>
2017-06-21 04:24:23 +0530

    correcting documentation

--- encrypt.go
+++ encrypt.go
@@ -22,15 +22,17 @@ func generateSalt(length int) []byte {
 	return salt
 }
 
-// EncryptPassword takes in a raw password as an argument, and returns its generated salt and encoded password.
-func EncryptPassword(rawPwd string) (string, string) {
+// EncryptPassword takes in a raw password as an argument, and returns its generated salt and encoded password,
+// with default options, unless a set of custom ones are provided.
+func EncryptPassword(rawPwd string, options ...interface{}) (string, string) {
 	salt := generateSalt(defaultSaltLen)
 	encodedPwd := pbkdf2.Key([]byte(rawPwd), salt, defaultIterations, defaultKeyLen, sha512.New)
 	return string(salt), hex.EncodeToString(encodedPwd)
 }
 
 // VerifyPassword takes three arguments, the raw password, its generated salt, and the encoded password,
-// and returns a boolean value determining whether the password is the correct one or not.
+// and returns a boolean value determining whether the password is the correct one or not, verifying with
+// default options, unless a set of custom ones are provided.
 func VerifyPassword(rawPwd string, salt string, encodedPwd string) bool {
 	return encodedPwd == hex.EncodeToString(pbkdf2.Key([]byte(rawPwd), []byte(salt), defaultIterations, defaultKeyLen, sha512.New))
 }
